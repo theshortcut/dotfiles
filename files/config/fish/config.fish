40b9ba411e9c950fdb1d2b298d7a9a0ab9db287f
Clayton Ferris <cferris@gmail.com>
2015-08-31 12:09:33 -0700

    updates for new oh my fish release

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,16 +1,16 @@
 # Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+set -g OMF_PATH $HOME/.oh-my-fish
 
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+# Path to your oh-my-fish configuration.
+set -g OMF_CONFIG $HOME/.config/omf
+
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
+# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
+# to create a custom plugin instead.
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Custom plugins and themes may be added to ~/.oh-my-fish/custom
-# Plugins and themes can be found at https://github.com/oh-my-fish/
-Theme 'bobthefish'
-Plugin 'theme'
+source $OMF_PATH/init.fish
 
 set GOPATH ~/.go
 set NVM_SYMLINK_CURRENT true
@@ -23,18 +23,4 @@ nvm use --silent default
 
 set PATH ~/bin $NVM_BIN /usr/local/bin /usr/local/sbin $GOPATH/bin $PATH
 
-
 alias g "git"
-
-set TF_ALIAS fuck
-function fuck -d 'Correct your previous console command'
-    set -l exit_code $status
-    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
-    set -l fucked_up_commandd $history[1]
-    thefuck $fucked_up_commandd > $eval_script
-    . $eval_script
-    rm $eval_script
-    if test $exit_code -ne 0
-        history --delete $fucked_up_commandd
-    end
-end
