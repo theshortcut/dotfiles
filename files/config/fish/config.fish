f38bd8db477f51580c1af7640e2c07f2ac13c09e
Clayton Ferris <cferris@gmail.com>
2016-12-09 16:01:00 -0700

    update fish configs

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,13 +1,21 @@
 set -x GOPATH ~/.go
 set -x NVM_SYMLINK_CURRENT true
 
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
+set -x PATH ~/bin $NVM_BIN /usr/local/bin $GOPATH/bin $PATH
 
-set -x PATH ~/bin $NVM_BIN /usr/local/bin /usr/local/sbin $GOPATH/bin $PATH
+# Scala
+set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
+
+# NVM
+nvm use default --silent
+
+# Android Studio
+set -x ANDROID_HOME ~/Library/Android/sdk/
+set -x PATH ~/Library/Android/sdk/platform-tools/ $PATH
 
 # SpinGo
 bass source $HOME/.bash.d/ports.bash
 
 alias g "git"
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
