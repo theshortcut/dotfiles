a4474d05fc354a8e3f042b626c93d66d42a3b69a
Clayton Ferris <cferris@gmail.com>
2017-06-05 10:00:17 -0700

    update for new tmux and fish stuff

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x GOPATH ~/.go
 set -x NVM_SYMLINK_CURRENT true
 
-set -x PATH ~/bin $NVM_BIN /usr/local/bin $GOPATH/bin $PATH
+set -x PATH ~/bin $NVM_BIN /usr/local/bin $HOME/.cargo/bin $GOPATH/bin $HOME/Library/Android/sdk/platform-tools $PATH
 
 # Scala
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
@@ -11,11 +11,16 @@ nvm use default --silent
 
 # Android Studio
 set -x ANDROID_HOME ~/Library/Android/sdk/
-set -x PATH ~/Library/Android/sdk/platform-tools/ $PATH
 
 # SpinGo
 bass source $HOME/.bash.d/ports.bash
 
 alias g "git"
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+# Theme Config
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme base16-dark
+
+# Editor
+# set -x EDITOR emacs
