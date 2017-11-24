644de810aef80b4f863b807a1578231754c7cff4
Clayton Ferris <cferris@gmail.com>
2017-11-23 20:49:33 -0700

    swap nvm for asdf

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,19 +1,13 @@
+set -x GPG_TTY (tty)
 set -x GOPATH ~/.go
-set -x NVM_SYMLINK_CURRENT true
 
-set -x PATH ~/bin $NVM_BIN /usr/local/bin $HOME/.cargo/bin $GOPATH/bin $HOME/Library/Android/sdk/platform-tools $PATH
+set -x PATH ~/bin $HOME/.cargo/bin $GOPATH/bin $HOME/Library/Android/sdk/platform-tools $PATH
 
 # Scala
 set -x SBT_OPTS "-Xms512M -Xmx2G -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
-# NVM
-nvm use default --silent
-
 # Android Studio
 set -x ANDROID_HOME ~/Library/Android/sdk/
-
-# SpinGo
-bass source $HOME/.bash.d/ports.bash
 
 alias g "git"
 
@@ -23,4 +17,4 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme base16-dark
 
 # Editor
-# set -x EDITOR emacs
+set -x EDITOR code
