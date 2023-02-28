2fc5295a2dbadbe5a3f5d0a2c95ccc19208de265
Clayton Ferris <cferris@gmail.com>
2023-02-28 09:30:02 -0700

    add coreutils, curl, delta, ripgrep, watchman, and wget

--- packages.nix
+++ packages.nix
@@ -1,13 +1,19 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.bat
+    pkgs.coreutils
+    pkgs.curl
+    pkgs.delta
     pkgs.exa
     pkgs.fish
     pkgs.fnm
     pkgs.git
     pkgs.jq
     pkgs.neovim
+    pkgs.ripgrep
     pkgs.starship
     pkgs.tmux
+    pkgs.watchman
+    pkgs.wget
   ];
 }
