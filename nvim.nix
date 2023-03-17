c3d526a563ce18e26053495aba354c928888d040
Clayton Ferris <cferris@gmail.com>
2023-03-17 15:06:50 -0700

    update flakes and nvim config

--- nvim.nix
+++ nvim.nix
@@ -3,8 +3,8 @@
     source = pkgs.fetchFromGitHub {
       owner = "theshortcut";
       repo = "neovim-config";
-      rev = "853e8ad92b436e6045ead6b154411c648ca3538b";
-      sha256 =  "yjq2QRJiUe69yRXakIKwIW8k+R4H1LYQXYZugdGVaFk=";
+      rev = "7485f2ae787a7ed8dc9b2115d9a2e0a49f3f776c";
+      sha256 = "1SNv+dZ65HyQpxF8Gu+oz/INOp87sPh8C6/uCYK9Om0=";
     };
   };
 }
