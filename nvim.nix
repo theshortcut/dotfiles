4c91e2d60fd2397b211cb83b688df1a23b10c8e1
Clayton Ferris <cferris@gmail.com>
2023-05-01 09:07:18 -0700

    bump

--- nvim.nix
+++ nvim.nix
@@ -3,8 +3,8 @@
     source = pkgs.fetchFromGitHub {
       owner = "theshortcut";
       repo = "neovim-config";
-      rev = "7485f2ae787a7ed8dc9b2115d9a2e0a49f3f776c";
-      sha256 = "1SNv+dZ65HyQpxF8Gu+oz/INOp87sPh8C6/uCYK9Om0=";
+      rev = "cee65bb10b7d49d2563ead7068e2f8ae567c40fa";
+      sha256 = "f/Jt87pxC3aMqe3BTrmMCZlIGSURjyeoxyCYojSsLbE=";
     };
   };
 }
