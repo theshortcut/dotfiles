7d749848fdc40a1c0788eeef82fca687bdda7818
Clayton Ferris <cferris@gmail.com>
2024-04-04 09:15:40 -0700

    add cargo bin to path and bump things

--- fish.nix
+++ fish.nix
@@ -23,7 +23,7 @@
     shellInit = ''
       set fish_greeting
       set -gx PNPM_HOME $HOME/.pnpm-global
-      set -gx PATH $PATH $PNPM_HOME
+      set -gx PATH $PATH $HOME/.cargo/bin $PNPM_HOME
     '';
     plugins = [
       {
