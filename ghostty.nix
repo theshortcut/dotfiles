9bd4aad1d9335325800e1990e5a2a38fba3c7ae2
Clayton Ferris <cferris@gmail.com>
2024-12-30 11:09:25 -0800

    add ghostty config

---
+++ ghostty.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib.generators) toKeyValue mkKeyValueDefault;
+
+in {
+  xdg.configFile."ghostty/config".text = toKeyValue { mkKeyValue = mkKeyValueDefault { } " = "; } {
+    font-family = "Iosevka Nerd Font Mono";
+    font-size = 14;
+    font-thicken = true;
+
+    adjust-cell-height = "20%";
+    adjust-cursor-height = "20%";
+    # adjust-cursor-thickness = "100%";
+    # adjust-box-thickness = "100%";
+    # adjust-underline-thickness = "400%";
+
+    theme = "light:catppuccin-latte,dark:catppuccin-macchiato";
+    window-theme = "system";
+    window-colorspace = "display-p3";
+    background-opacity = 0.9;
+    background-blur-radius = 10;
+    window-padding-x = 0;
+    window-padding-y = 0;
+
+    auto-update = "download";
+  };
+}
--- home.nix
+++ home.nix
@@ -3,5 +3,5 @@
   home.homeDirectory = "/Users/clay";
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
-  imports = [ ./packages.nix ./fish.nix ./git.nix ./kitty.nix ./nvim.nix ./starship.nix ./tmux.nix ./wezterm.nix ./rest.nix ];
+  imports = [ ./packages.nix ./fish.nix ./ghostty.nix ./git.nix ./kitty.nix ./nvim.nix ./starship.nix ./tmux.nix ./wezterm.nix ./rest.nix ];
 }
